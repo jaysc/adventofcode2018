4b373f706e4b946afb01defc0a244768797bbb95
Jaysc <jaysc2@gmail.com>
2018-12-02 22:24:56 +0000

    Day 2

---
+++ 2/1.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+count2 = 0
+count3 = 0
+with open("input.txt", "r") as file:
+    for line in file:
+        #Counter from collections module can count the number of times a letter appears
+        #We then store the values into a set(hashset)
+        count = set(Counter(line).values())
+
+        if 2 in count:
+            count2 += 1
+        if 3 in count:
+            count3 += 1
+
+    result = count2 * count3
+        
